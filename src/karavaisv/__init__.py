c6e1400913a1973290fde7a044ce1f16e70074e0
Денис Панин <lozejor12@gmail.com>
2025-10-15 00:18:59 +0300

    Feat[dev/1.0_ffw_render] Added dummy functions for reading sources

--- src/karavaisv/__init__.py
+++ src/karavaisv/__init__.py
@@ -1,2 +1,3 @@
 from .rendering import render
 from .main import main
+from .util_funcs import print_render_info_start, print_render_info_end, read_params_from_yaml, read_source_from_file
--- src/karavaisv/util_funcs.py
+++ src/karavaisv/util_funcs.py
@@ -1,3 +1,4 @@
+# functuons for printing rendering info
 
 def print_render_info_start(filename: str) -> None:
     if filename is not None:
@@ -12,3 +13,20 @@ def print_render_info_end(filename: str, ellapsed_time: float) -> None:
     else:
         print(f"Info: Rendering completed in {ellapsed_time:.2f} seconds.")
 
+
+# functions for reading source files
+
+def read_params_from_yaml(filepath: str) -> dict:
+    import yaml
+
+    with open(filepath, 'r') as file:
+        parameters = yaml.safe_load(file)
+
+    return parameters
+
+
+def read_source_from_file(filepath: str) -> str:
+    with open(filepath, 'r') as file:
+        source_code = file.read()
+
+    return source_code
