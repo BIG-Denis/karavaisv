ea42f1763031572f7c1172f6b71302daaee3a6fb
Денис Панин <lozejor12@gmail.com>
2025-10-14 23:53:04 +0300

    Feat[dev/1.0_ffw_render] Removed redunant code

--- src/karavaisv/rendering.py
+++ src/karavaisv/rendering.py
@@ -25,16 +25,6 @@ def check_line_for_meta(line: str) -> str:
     return None
 
 
-def is_meta_in_block(content: str) -> bool:
-    all_lines = content.split('\n')[:-1]
-
-    for line in all_lines:
-        if check_line_for_meta(line) is not None:
-            return True
-
-    return False
-
-
 def render_single_line(line: str, variables :dict) -> str:
     inline_templates: list[str] = re.findall(r"\~/.*?/\~", line)
     exec_locals: dict = variables
@@ -62,79 +52,6 @@ def meta_to_py_line(line: str) -> str:
         converted_line += ':'
 
     return converted_line
-
-
-def render_single_block(content: str, variables: dict) -> str:
-    all_lines: list[str] = content.split('\n')[:-1]
-    executable_content: str = ""
-    executed_content: str = ""
-    all_locals: dict = variables | locals()
-    exec_locals: dict = locals() | all_locals
-    block_contains_meta: bool = is_meta_in_block(content)
-
-    for line in all_lines:
-        if check_line_for_meta(line) is not None: # meta
-            executable_content += meta_to_py_line(line)
-
-        else:                                     # non-meta
-            if block_contains_meta:
-                executable_content += 4 * ' '
-            executable_content += "executed_content += "
-            executable_content += f'render_single_line("""{line}""", all_locals | locals())'
-
-        executable_content += '\n'
-
-    exec(executable_content, locals=exec_locals)
-    executed_content = exec_locals['executed_content']
-
-    return executed_content
-
-
-def divide_into_blocks(content: str) -> list[str]:
-    depth_level: int = 0
-    ksv_blocks: list[str] = []
-    line_skip: bool = False
-    all_lines: list[str] = content.split('\n')[:-1]
-
-    for i, line in enumerate(all_lines):
-        meta_key: str = check_line_for_meta(line)
-        line_skip = False
-
-        if meta_key in KSV_META_OPENINGS:
-            depth_level += 1
-            if depth_level == 1:
-                ksv_blocks.append("")
-
-        if meta_key in KSV_META_CLOSURES:
-            depth_level -= 1
-            if depth_level == 0:
-                ksv_blocks.append("")
-                line_skip = True
-
-        if not line_skip:
-            if len(ksv_blocks) == 0:
-                ksv_blocks.append("")
-            ksv_blocks[-1] += line
-            if (i != len(all_lines)):
-                ksv_blocks[-1] += '\n'
-
-    return ksv_blocks
-
-
-def render_substring(content: str, variables: dict) -> str:
-    rendered_content: str = ""
-    ksv_blocks: list[str] = divide_into_blocks(content)
-    ksv_blocks_count: int = len(ksv_blocks)
-
-    if ksv_blocks_count > 1:
-        for ksv_block in ksv_blocks:
-            rendered_content += render_substring(ksv_block, variables)
-
-    elif ksv_blocks_count == 1:
-        rendered_content += render_single_block(ksv_blocks[0], variables)
-
-    return rendered_content
-
 
 
 def templated_to_executable(content: str) -> str:
